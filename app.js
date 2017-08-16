95b85a6c76e1c56ec7184d7ac08fa8bfc99ba07f
Jorge Gallardo <jorge8486@gmail.com>
2017-08-16 18:22:08 -0400

    error handling

--- app.js
+++ app.js
@@ -16,10 +16,18 @@ const argv = yargs
 
 var encodedAddress = encodeURIComponent(argv.a);
 
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  //console.log(JSON.stringify(body, undefined, 2)); //pretty print
-  console.log(`Address: ${body.results[0].formatted_address}\nLatitude: ${body.results[0].geometry.location.lat}.\nLongitude: ${body.results[0].geometry.location.lng}`);
-});
+if(encodedAddress.length !== 0) {
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  }, (error, response, body) => {
+    if(error) {
+      console.log('Unable to connect to Google servers.');
+    } else if(body.status === 'INVALID_REQUEST' || body.status === 'ZERO_RESULTS') {
+      console.log('Unable to find address.');
+    } else if(body.status === 'OK') {
+      //console.log(JSON.stringify(body, undefined, 2)); //pretty print
+      console.log(`Address: ${body.results[0].formatted_address}\nLatitude: ${body.results[0].geometry.location.lat}.\nLongitude: ${body.results[0].geometry.location.lng}`);
+    }
+  });
+} else console.log('Address missing.');
